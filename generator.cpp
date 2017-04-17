88f0f3915ae533a65dfe9afe5eeabc15f914ad92
Julio Xavier <julioxavierr@live.com>
2017-04-16 23:02:46 -0300

    New generator

--- generator.cpp
+++ generator.cpp
@@ -1,12 +1,20 @@
-#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 int main(){
-	for (int i = 0; i < 1000; ++i)
-	{
-		cout << rand() % 1001 - 500 << endl;
+
+	ofstream file;
+	file.open("input");
+
+	for (int i = 0; i < 1000; ++i){
+		file << rand() % 1001 - 500 << endl;
 	}
+
+	file << "end" << endl;
+	
+	file.close();
 
 	return 0;
 }
