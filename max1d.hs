304d5a64fe2f91a4c199ab0c080d4fb2603331a4
VinyPinheiro <viny-pinheiro@hotmail.com>
2017-04-16 21:49:05 -0300

    Haskell code with the Max1D Range Sum implementation

---
+++ max1d.hs
@@ -0,0 +1,31 @@
+import Data.Char  
+import qualified Data.List  
+  
+main = do
+    putStrLn "Entre com a Lista"
+    rs <- getAllLines
+
+    putStrLn (show (max1d (stringToInt rs)))
+
+    putStrLn ("\nObrigado por utilizar")
+
+stringToInt :: [[Char]] -> [Int]
+stringToInt [] = []
+stringToInt [x] = [read x :: Int]
+stringToInt valor = [read (head valor) :: Int] ++ stringToInt (tail valor)
+
+max1d :: (Ord a, Num a) => [a] -> a
+max1d xs = maximum (map sum (subsequences xs))
+
+
+subsequences :: [a] -> [[a]]
+subsequences = concatMap Data.List.inits . Data.List.tails
+
+getAllLines :: IO [String]
+getAllLines = do
+  x <- getLine
+  if x == "end"
+    then return []
+    else do
+      xs <- getAllLines
+      return (x:xs)
